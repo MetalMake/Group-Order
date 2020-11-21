33c185f688795953036619cf81893398d9826d7a
MetalMake <59830249+MetalMake@users.noreply.github.com>
2020-11-21 22:04:29 +0200

    Toimii

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,14 +14,8 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  }
+
+
 ];
 @NgModule({
   imports: [
