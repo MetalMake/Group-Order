1e924f2ed3a081c3908eae7ecb92d5a9183e9846
MetalMake <59830249+MetalMake@users.noreply.github.com>
2020-11-21 22:38:11 +0200

    Toimii?
    werwercvb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
