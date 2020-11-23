d207df2487121a52119ec03fb1c05d87daeeb3e4
Mikko <mikko.karhavirta@student.laurea.fi>
2020-11-23 18:56:54 +0200

    tabi login sivulla

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
+import { TabsPageRoutingModule } from '../tabs/tabs-routing.module';
+import { Tab1PageRoutingModule } from '../tab1/tab1-routing.module';
 
 import { LoginPage } from './login.page';
+import { TabsPage } from '../tabs/tabs.page';
+import { Tab1Page } from '../tab1/tab1.page';
+
+
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPage
+  },
+  {
+    path: 'tabs',
+    component: TabsPage
   }
 ];
 
