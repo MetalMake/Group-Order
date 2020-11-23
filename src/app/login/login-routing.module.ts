64c893e3ce6ae2bb423931bf65d378ebb45b16a6
Mikko <mikko.karhavirta@student.laurea.fi>
2020-11-23 19:02:32 +0200

    tabit loginissa toimivat

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppRoutingModule } from '../app-routing.module';
-import { TabsPageRoutingModule } from '../tabs/tabs-routing.module';
-import { Tab1PageRoutingModule } from '../tab1/tab1-routing.module';
 
 import { LoginPage } from './login.page';
-import { TabsPage } from '../tabs/tabs.page';
 import { Tab1Page } from '../tab1/tab1.page';
-
+import { Tab2Page } from '../tab2/tab2.page';
+import { Tab3Page } from '../tab3/tab3.page';
 
 
 const routes: Routes = [
@@ -16,8 +13,16 @@ const routes: Routes = [
     component: LoginPage
   },
   {
-    path: 'tabs',
-    component: TabsPage
+    path: 'tab1',
+    component: Tab1Page
+  },
+  {
+    path: 'tab2',
+    component: Tab2Page
+  },
+  {
+    path: 'tab3',
+    component: Tab3Page
   }
 ];
 
