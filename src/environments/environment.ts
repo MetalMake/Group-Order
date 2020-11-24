3cd294343758218d05457e07a564d3c4aa2e50cd
Mikko <mikko.karhavirta@student.laurea.fi>
2020-11-24 20:18:56 +0200

    measurement id added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyB8d2aWbc0udBxjFHXziLm72ZwxDXnjXMo",
-    authDomain: "41497973450-1f1gvvga989d4j3p5tsn8ree5fqs3k1g.apps.googleusercontent.com",
-    databaseURL: "https://group-order-2651c.firebaseio.com",
-    projectId: "group-order-2651c",
-    storageBucket: "group-order-2651c.appspot.com",
-    messagingSenderId: "41497973450",
-    appId: "1:41497973450:android:90081c33455049287b5a33",
-    measurementId: "<your-measurement-id>"
+    apiKey: 'AIzaSyB8d2aWbc0udBxjFHXziLm72ZwxDXnjXMo',
+    authDomain: '41497973450-1f1gvvga989d4j3p5tsn8ree5fqs3k1g.apps.googleusercontent.com',
+    databaseURL: 'https://group-order-2651c.firebaseio.com',
+    projectId: 'group-order-2651c',
+    storageBucket: 'group-order-2651c.appspot.com',
+    messagingSenderId: '41497973450',
+    appId: '1:41497973450:android:90081c33455049287b5a33',
+    measurementId: 'G-W4D67WNWEX'
   }
 };
 
