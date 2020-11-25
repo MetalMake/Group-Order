f83f09809567642179281d428430ea519f957fff
Mikko <mikko.karhavirta@student.laurea.fi>
2020-11-25 21:42:41 +0200

    tab1 sotkettu

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../shared/Course';
+import { CourseService } from './../shared/course.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
 
+  Courses = [];
+
+  constructor(
+    private courseService: CourseService
+  ) { }
+
+  ngOnInit() {
+    this.fetchCourses();
+    const courseRes = this.courseService.getCourseList();
+    courseRes.snapshotChanges().subscribe(res => {
+      this.Courses = [];
+      res.forEach(item => {
+        const a = item.payload.toJSON();
+        // tslint:disable-next-line: no-string-literal
+        a['$key'] = item.key;
+        this.Courses.push(a as Course);
+      });
+    });
+  }
+
+fetchCourses() {
+  this.courseService.getCourseList().valueChanges().subscribe(res => {
+    console.log(res);
+  });
 }
+
+deleteCourse(id) {
+  console.log(id);
+  if (window.confirm('Do you really want to delete?')) {
+    this.courseService.deleteCourse(id);
+  }
+}
+}
+
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'create-course',
+        loadChildren: () => import('../create-course/create-course.module').then(m => m.CreateCoursePageModule)
+      },
+      {
         path: '',
         redirectTo: 'tabs/tab1',
         pathMatch: 'full'
